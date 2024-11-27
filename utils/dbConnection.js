db0294f683c16bb02ca5ee079744e930b1942423
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2024-11-27 22:14:43 +0900

    커넥션 조정

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,16 +1,21 @@
 const mariadb = require('mariadb');
 
-// MariaDB 연결 풀 생성 함수
+let pool; // 전역 Pool 변수
+
 function createConnectionPool() {
-  return mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
-   });
+  if (!pool) {
+    // Pool이 없으면 생성
+    pool = mariadb.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      connectionLimit: 5, // 동시 연결 수 제한
+      idleTimeout: 10000, // 10초 비활성화 상태 후 종료
+          });
+  }
+  return pool;
 }
 
-// 다른 파일에서 사용할 수 있도록 내보냄
 module.exports = createConnectionPool;
